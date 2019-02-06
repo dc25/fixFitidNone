e57e702774a001ba0a59f40d7debe431b0486426
Dave Compton <davecompton7@gmail.com>
2019-02-05 19:02:37 -0800

    changed comments

--- fixChaseFitidNone.py
+++ fixChaseFitidNone.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python 
 # Python program to fix up the Chase bad QFX export FITID=NONE
-# hleofxquotes@gmail.com
+# modified version of : fixChaseFitidNone.py by hleofxquotes@gmail.com
+# (see https://social.microsoft.com/Forums/mvpforum/en-US/f9a4fa77-fe71-4eed-a66e-c828572ab911/fixchasefitidnonepy-python-script-to-fix-up-chase-ltfitidgtnone?forum=money)
 # python fixChaseFitidNone.py Checking1.qfx 
 #
 from __future__ import print_function   # If code has to work in Python 2 and 3!
