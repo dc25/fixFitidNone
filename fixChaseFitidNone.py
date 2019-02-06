6af5b8c86b3a4b47a0f3f13bd4358489963c5cbc
Dave Compton <davecompton7@gmail.com>
2019-02-05 17:33:11 -0800

    edit in place

--- fixChaseFitidNone.py
+++ fixChaseFitidNone.py
@@ -1,48 +1,21 @@
+#! /usr/bin/python 
 # Python program to fix up the Chase bad QFX export FITID=NONE
 # hleofxquotes@gmail.com
-# Usage:
-# python fixChaseFitidNone.py -i Checking1.qfx -o out.qfx
+# python fixChaseFitidNone.py Checking1.qfx 
 #
 from __future__ import print_function   # If code has to work in Python 2 and 3!
 
-import sys, getopt
+import sys
+import fileinput
+from shutil import copyfile
 import hashlib
 
-def usage():
-  print ('fixChaseFitidNone.py -i <inputFile> -o <outputFile>')
-
 def main(argv):
-  inputFile = ''
-  outputFile = ''
-
-  try:
-    opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-  except getopt.GetoptError:
-    usage()
-    sys.exit(2)
-  for opt, arg in opts:
-    if opt == '-h':
-      usage()
-      sys.exit()
-    elif opt in ("-i", "--ifile"):
-      inputFile = arg
-    elif opt in ("-o", "--ofile"):
-      outputFile = arg
-
-  if not inputFile:
-    usage()
-    sys.exit(2)
-  if not outputFile:
-    usage()
-    sys.exit(2)
-
-  file = open(inputFile, "r")
-  print ('> Reading from', inputFile)
-  ofile = open(outputFile, "w")
-  print ('< Writing to', outputFile)
+  qfxfile=sys.argv[1]
+  copyfile(qfxfile, qfxfile+".original")
 
   inTransaction = False
-  for line in file:
+  for line in fileinput.input(qfxfile, inplace = True):
     line = line.strip()
 
     if line == '<STMTTRN>':
@@ -60,9 +33,9 @@ def main(argv):
           for item in transaction:
             if item == '<FITID>NONE':
               item = '<FITID>' + hashlib.md5(longString.encode()).hexdigest()
-            ofile.write(item + "\n")
+            print(item)
     else:
-        ofile.write(line + "\n")
+        print(line)
 
 
 if __name__ == "__main__":
